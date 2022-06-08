4c1c3f82b29d3d2bbc553d501ed3d9585ff0dd0c
Alexandre Bastos <xande.bastos321@gmail.com>
2022-06-07 23:04:58 -0300

    feita validação de email na pagina de login

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+$("#recuperar").on("click", function(){
+    let email = $("#email").val()
+    let emailConnfirn = $("#email-confirm").val()
+    let teste = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+    let resultado = teste.test(email)
+
+    if(resultado == false) {
+        alert("Digite um e-mail valido")
+    } 
+})
